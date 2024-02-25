52b92f4e5415e02055b8a4b49b43327e685032b7
visstt <egorskomorohov020606@gmail.com>
2024-02-25 11:14:50 +0500

    add email

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './SearchResult.css'
+
 export const SearchResult = ({result}) => {
   return (
-    <div className='search-result' onClick={(e)=> alert(`you clicked on ${result.name}`)}>{result.name}</div>
+    <div className='search-result' onClick={(e)=> alert(`you clicked on ${result.name}, \n email: ${result.email}`)}>{result.name}</div>
   )
 }
